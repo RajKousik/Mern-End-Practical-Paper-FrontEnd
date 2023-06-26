2ef54af5f7a9ed2be9ee0b550d108616483ae27b
Raj Kousik <rajkousik20@gmail.com>
2023-06-26 09:50:39 +0530

    Cart Page Added

---
+++ src/components/GetAllProductsComponent/GetAllProductsComponent.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import './GetAllProductsComponent.css'
+import ProductComponent from '../ProductComponent/ProductComponent'
+
+
+function GetAllProductsComponent() {
+    const [products, setProducts] = useState([])
+
+    const fetchAllProducts  = async() => {
+        const response = await fetch('http://localhost:3500/api/v1/shoppingCart/')
+        const data = await response.json()
+        console.log(data);
+        setProducts(data)
+    }
+
+    useEffect(() => {
+        fetchAllProducts()
+    }, [])
+
+  return (
+    <div className='products'>
+        {/* <p>Hello For Product C</p> */}
+        {products.map(productItem=>(
+            <ProductComponent productItem={productItem}/>
+            
+        ))}
+    </div>
+  )
+}
+
+export default GetAllProductsComponent
